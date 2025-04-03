bd34cf220f236e9f765eee99cbb1e343c6ca5def
HP CA <murayansima@gmail.com>
2025-04-03 18:45:29 +0200

    Commit #1
    In NV, from line 4 to line 26, I  create the MercedesBenz class which reflets the addition of the class, its price calculation, and its currency conversion functionality.

--- ExchangeRates/src/main/java/exchangerate/rates.java
+++ ExchangeRates/src/main/java/exchangerate/rates.java
@@ -1,7 +1,7 @@
 package exchangerate;
 
 public class rates {
-    static final double UGX_to_USD = 0.00035;
+    static final double UGX_to_USD = 0.000350;
     static final double USD_to_UGX= 3500;
     public static double displayRates(String from, String to){
        if (from.equals("UGX") &&  to.equals("USD") ){
--- Vehicle/src/main/java/Car/MercedesBenz.java
+++ Vehicle/src/main/java/Car/MercedesBenz.java
@@ -1,4 +1,30 @@
 package Car;
-
+import exchangerate.rates;
 public class MercedesBenz {
+    String MercedesBenzBrand;
+    int MercedesBenzId;
+    double MercedesBenzPrice;
+    String ClearanceDay;
+
+    public MercedesBenz(String MercedesBenzBrand, int MercedesBenzId, double MercedesBenzPrice, String ClearanceDay) {
+        this.MercedesBenzBrand = MercedesBenzBrand;
+        this.MercedesBenzId = MercedesBenzId;
+        this.MercedesBenzPrice = MercedesBenzPrice;
+        this.ClearanceDay = ClearanceDay;
+    }
+
+    public double GetMercedesBenzCost(int NumberOfCar) {
+        double cost = MercedesBenzPrice * NumberOfCar;
+        return cost;
+    }
+
+    public <GetMercedesBenzCost> double ExchangeRates(String from, String to, int NumberOfCar) {
+        double ActualRates = rates.displayRates(from, to);
+        double cost = GetMercedesBenzCost(NumberOfCar);
+        double ActualCost;
+        ActualCost = cost;
+        return ActualRates * ActualCost;
+    }
 }
+
+
