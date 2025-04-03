167cc93158e9f662aad58bd564fee47a3ba92ffa
HP CA <murayansima@gmail.com>
2025-04-03 17:48:36 +0200

    Commit #1
    In NV,  from line 4 to line 15, I create the addition of a rates class with static exchange rate constants and a method for simple currency conversion.

--- ExchangeRates/src/main/java/exchangerate/rates.java
+++ ExchangeRates/src/main/java/exchangerate/rates.java
@@ -1,4 +1,16 @@
 package exchangerate;
 
 public class rates {
+    static final double UGX_to_USD = 0.00035;
+    static final double USD_to_UGX= 3500;
+    public static double displayRates(String from, String to){
+       if (from.equals("UGX") &&  to.equals("USD") ){
+           return UGX_to_USD;
+       } else if (from.equals("USD") && to.equals("UGX")) {
+           return USD_to_UGX;
+       }
+       else {
+           return 0;
+       }
+    }
 }
